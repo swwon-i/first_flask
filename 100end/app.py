5e74b71f89640f333efa874be8bc8bf8f1ecd92b
swwon-i <stonesw123@naver.com>
2025-06-11 14:39:12 +0900

    'app_수정

--- 100end/app.py
+++ 100end/app.py
@@ -9,5 +9,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
