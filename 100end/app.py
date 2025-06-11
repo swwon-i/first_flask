18b246d9a05941b5deaaa99742e6fc7510f2178f
swwon-i <stonesw123@naver.com>
2025-06-11 16:23:27 +0900

    readme

--- 100end/app.py
+++ 100end/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
 # 객체 생성
 app = Flask(__name__)
+app.secret_key = 'tjrdnjs'
 
 # 기본 루트
 ## 실행하자마자자
@@ -16,6 +17,27 @@ def about():
 @app.route('/projects')
 def projects():
     return render_template('projects.html')
+
+# GET : 초기에 경로로 들어가면 작성 폼을 보여주자
+# POST : 입력받은 폼 가지고 뭔가를 보여주자
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method== 'POST':
+        name = request.form.get('name')
+        mail = request.form.get('email')
+        message = request.form.get('message')
+        print(name, mail, message)
+        
+        # 그냥 패턴 readme 참조
+        # return render_template('contact.html', success=True)
+        
+        # PRG 패턴
+        session['success'] = True
+        return redirect(url_for('contact'))
+    
+    # get 방식
+    success = session.pop('success', False)
+    return render_template('contact.html', success=success)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-"# first_flask" 
+```
+flask는
+새로 고침이나 페이지를 열면 success = True가 남아이씀
+단순히 렌더링한다고 생각 -> 이전 정보를 그대로 사용
+```
+
+# PRG 패턴
+```
+Post -> Redirect -> Get
+Post 요청 처리 후 사용자를 리다이렉트 시켜서 GET 요청으로 바꿔줌
+```
+
+### PRG 설명
+1. 초기에는 success가 False
+    a. get 방식으로 화면을 보여줌
+    b. 사용자가 폼을 입력해서 제출
+2. POST방식으로 입력 제출
+    a. success가 True로 변경
+    b. 성공 메시지 출력
+    c. get방식으로 화면 보여줌
+3. 새로고침시 다시 contact 함수 실행
+    a. 존재했던 success 정보를 지움
+    b. 1번으로 이동
